77af09f7ea623950cd5504f1e5a1a9a07a33f584
50u7h <50u7h@protonmail.com>
2023-03-13 17:12:15 +0300

    check-if-student-is-null-test

--- src/test/java/com/guney/springmvc/StudentAndGradeServiceTest.java
+++ src/test/java/com/guney/springmvc/StudentAndGradeServiceTest.java
@@ -3,22 +3,39 @@ package com.guney.springmvc;
 import com.guney.springmvc.models.CollegeStudent;
 import com.guney.springmvc.repository.StudentDao;
 import com.guney.springmvc.service.StudentAndGradeService;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.test.context.TestPropertySource;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
 
 @TestPropertySource("/application.properties")
 @SpringBootTest
 public class StudentAndGradeServiceTest {
 
     @Autowired
+    private JdbcTemplate jdbc;
+
+    @Autowired
     private StudentAndGradeService studentAndGradeService;
 
     @Autowired
     private StudentDao studentDao;
+
+    @BeforeEach
+    public void setupDatabase() {
+        jdbc.execute("insert into student(id, firstname, lastname, email_address) " +
+                "values (1, 'Eric', 'Roby', 'eric.roby@guney.com')");
+    }
+
+    @AfterEach
+    public void setupAfterTransaction() {
+        jdbc.execute("DELETE FROM student");
+    }
 
     @Test
     public void createStudentService() {
@@ -29,4 +46,10 @@ public class StudentAndGradeServiceTest {
         assertEquals("tset@tset.com", student.getEmailAddress(), "find by email");
     }
 
+    @Test
+    public void isStudentNullCheck() {
+        assertTrue(studentAndGradeService.checkIfStudentIsNull(1));
+
+        assertFalse(studentAndGradeService.checkIfStudentIsNull(0));
+    }
 }
