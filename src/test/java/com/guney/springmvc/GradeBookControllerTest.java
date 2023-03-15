b09e1ec7ea639f08a3ad82d98578ab6937a66357
50u7h <50u7h@protonmail.com>
2023-03-16 00:51:51 +0300

    delete-a-non-valid-grade-http-request

--- src/test/java/com/guney/springmvc/GradeBookControllerTest.java
+++ src/test/java/com/guney/springmvc/GradeBookControllerTest.java
@@ -294,4 +294,16 @@ public class GradeBookControllerTest {
 
         ModelAndViewAssert.assertViewName(mav, "error");
     }
+
+    @Test
+    public void deleteANonValidGradeHttpRequest() throws Exception {
+
+        MvcResult mvcResult = mockMvc.perform(MockMvcRequestBuilders
+                        .get("/grades/{id}/{gradeType}", 1, "literature"))
+                .andExpect(status().isOk()).andReturn();
+
+        ModelAndView mav = mvcResult.getModelAndView();
+
+        ModelAndViewAssert.assertViewName(mav, "error");
+    }
 }
