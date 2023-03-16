6c075e18c3fb33a6d513fa23cc5e6feac5a50dcd
50u7h <50u7h@protonmail.com>
2023-03-16 15:58:38 +0300

    create-a-valid-student-id-does-not-exist

--- src/test/java/com/guney/springmvc/GradeBookControllerTest.java
+++ src/test/java/com/guney/springmvc/GradeBookControllerTest.java
@@ -230,6 +230,19 @@ public class GradeBookControllerTest {
                 .andExpect(jsonPath("$.emailAddress", is("eric.roby@guney.com")))
                 .andExpect(jsonPath("$.studentGrades.mathGradeResults", hasSize(2)));
     }
+
+    @Test
+    public void createAValidGradeHttpRequestStudentDoesNotExistEmptyResponse() throws Exception {
+
+        mockMvc.perform(post("/grades")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .param("grade", "85.00")
+                        .param("gradeType", "math")
+                        .param("studentId", "0"))
+                .andExpect(status().is4xxClientError())
+                .andExpect(jsonPath("$.status", is(404)))
+                .andExpect(jsonPath("$.message", is("Student or Grade was not found")));
+    }
 }
 
 
